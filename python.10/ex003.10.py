e0f5052647e9c02a8343446b5ab0036acb886eb5
moh_plada <117128136+MohPlada@users.noreply.github.com>
2022-12-19 00:09:37 -0300

    Add files via upload

---
+++ python.10/ex003.10.py
@@ -0,0 +1,24 @@
+"""[Exercício 4] Escreva um programa que
+vai ler vários valores numéricos e colocar
+eles em uma lista. No final, mostre:
+1)	Quantos números foram digitados;
+2)	A lista de valores digitados, ordenada de forma crescente;
+3)	Se o valor 5 foi digitado e está ou não na lista."""
+
+numeros = list()
+while True:
+    numeros.append(int(input("Digite um valor: ")))
+
+    resposta = str(input("Deseja continuar [S/N]:")).upper().strip()[0]
+    while resposta not in 'SN':
+        resposta = str(input("RESPOSTA INVÁLIDA! \nDeseja continuar [S/N]:")).upper().strip()[0]
+    if resposta == 'N':
+        break
+
+numeros.sort(reverse=True)
+print(f"Você digitou {len(numeros)} números.")
+print(f"A lista que você digitou em ordem decrescente é {numeros}")
+if 5 in numeros:
+    print("O valor 5 faz parte da lista.")
+else:
+    print("O valor 5 \033[031mNÃO\033[m faz parte da lista.")
